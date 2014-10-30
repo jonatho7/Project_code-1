0681e9a8c3599f94562c9eccad71a9a3a6b16586
Jonathon Hellmann <jonatho7@vt.edu>
2014-10-30 11:01:56 -0400

    redirect for users profile

---
+++ app/controller/user.php
@@ -0,0 +1,36 @@
+<?php
+    	
+    require_once '../global.php';
+    
+    require_once 'sessionAttributes.php';
+    require_once '../model/User.class.php';
+    require_once '../model/UserState.class.php';
+    
+    
+    
+   if (empty($_SESSION['userName'])) {
+    	# Redirect as user is not logged in.
+    	session_unset();
+    	require_once 'login.php';
+    	exit();
+    }
+    
+
+    //echo "UserName = ". $_SESSION['userName'];
+    $user = User::getUserByUserName($_SESSION['userName']);
+    
+    if ($user == NULL) {
+        # CHANGE ME to login folder.
+        header('Location: '. SERVER_PATH. 'test');
+        exit();
+    }
+    
+    # Set the title and user object
+    $e_Title = "Profile";
+    $e_user = $user;
+    
+    require_once '../view/loggedInHeader.tpl';
+    require_once '../view/profile.tpl';
+	require_once '../view/loggedInFooter.tpl';
+
+?>
