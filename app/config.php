187a70f70485fb5d6dad9fc0eeaf365cf1b8dc14
vivekb88 <vivekb88@vt.edu>
2014-11-08 17:33:16 -0500

    adding code for AMT worker code flow. Rudimentary form

--- app/config.php
+++ app/config.php
@@ -10,4 +10,11 @@ define('DB_PASS','buddY181');
 define('DB_DATABASE','flu');
 
 //Variable stored in Session for tracking state of each user.
-define('USER_STATE', 'userState'); 
+define('USER_STATE', 'userState');
+
+//Change this parameter depending on where the results need to be submitted
+//for Amazon Mechanical Turks
+define("AMT_SUBMIF_FORM", "http://localhost/flutracker/test.php");
+define("PREVIEW_HIT", "ASSIGNMENT_ID_NOT_AVAILABLE");
+define("ASSIGNMENT_ID", "assignmentId");
+define("WORKER_ID", "workerId");
---
+++ app/controller/amtWorker.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once '../config.php';
+require_once '../model/User.class.php';
+
+/*
+ * This is a page that would be displayed only for Amazon
+ * Mechanical Turks worker only. 
+ * 
+ * i) Authentication is bypassed
+ * 
+ * ii) If worker is previewing the hit, sample test will be shown
+ * 
+ * iii) If the worker has accepted the hit, then a new user with
+ * the following paramaters <username, password> = <workerId, workerID>
+ * in the dabasase.
+ * 
+ * To do: Figure out what else to cache.
+ * 
+ */
+
+#echo "Hello AMT worker";
+
+/*
+ * Need to add more checkings later
+ */ 
+
+if (empty($_GET["assignmentId"])) {
+	echo "Unauthorized access. Access only for AMT workers";
+	exit();
+} 
+
+
+$assignmentID = $_GET[ASSIGNMENT_ID];
+
+if ($assignmentID == PREVIEW_HIT) {
+	require_once '../view/previewHit.tpl';
+	exit();
+} else {
+	/*
+	 * Add a user Id if user doesn't already exists
+	 * and start providing him the tasks.
+	 */
+	if (empty($_GET[WORKER_ID])) {
+		echo "workerID is null. Exiting";
+		exit();
+	}
+	$workerId = $_GET[WORKER_ID];
+	
+	$user = User::getUserByUserName($workerId);
+	if ($user == null) {
+		echo "user doesn't exist so creating a new one";
+	}
+	
+	
+}
