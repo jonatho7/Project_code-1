dee57b2d73cdd894af3e7c2f4e94d42a9f5f7672
root <root@ip-172-31-22-100.us-west-2.compute.internal>
2014-11-12 01:36:14 +0000

    fixing the broken link

--- app/config.php
+++ app/config.php
@@ -1,23 +1,23 @@
 <?php
 
-define('SERVER_PATH','http://localhost/flutracker/');
+#define('SERVER_PATH','http://localhost/flutracker/');
 
 // Path for setting up on Amazon EC2
-#define('SERVER_PATH','https://ec2-54-69-66-88.us-west-2.compute.amazonaws.com/flutracker/');
+define('SERVER_PATH','https://ec2-54-69-66-88.us-west-2.compute.amazonaws.com/flutracker/');
 
 // database constants
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','buddY181');
-define('DB_DATABASE','flu');
+define('DB_DATABASE','flu2');
 
 //Variable stored in Session for tracking state of each user.
 define('USER_STATE', 'userState');
 
 //Change this parameter depending on where the results need to be submitted
 //for Amazon Mechanical Turks
-define("AMT_SUBMIT_FORM", SERVER_PATH. "test.php");
-#define("AMT_SUBMIT_FORM", "https://workersandbox.mturk.com/mturk/externalSubmit");
+#define("AMT_SUBMIT_FORM", SERVER_PATH. "test.php");
+define("AMT_SUBMIT_FORM", "https://www.mturk.com/mturk/externalSubmit");
 
 define("PREVIEW_HIT", "ASSIGNMENT_ID_NOT_AVAILABLE");
 define("ASSIGNMENT_ID", "assignmentId");
